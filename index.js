30b59dc007524b1bd0e6b1f43bbe8cfb70293b0f
David Braden <davidbraden@gmail.com>
2016-08-06 12:00:34 +0100

    Add proxy to api for CORS

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var apiBase = 'https://young-hamlet-85537.herokuapp.com'
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -8,6 +10,11 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response) {
    response.send('Hello World!');
 });
+
+
+
+var proxy = require('express-http-proxy');
+app.use('/proxy', proxy(apiBase));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
